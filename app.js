333ae6459c427f14c37564036155163ae2144221
Onur Dogan <iamonurdogan@gmail.com>
2021-10-23 14:55:23 +0300

    header section completed

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+window.onscroll = () => {
+
+  if(window.scrollY > 100) {
+    document.querySelector('header').classList.add('active');
+  }else{
+    document.querySelector('header').classList.remove('active');
+  }
+
+  }
