b83e8eacf1aad7bf5b77f8c627dd80d63d2ac8e0
Onur Dogan <iamonurdogan@gmail.com>
2021-10-24 14:34:40 +0300

    navbar section completed

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+//Start of Navbar Section //
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-bar').onclick = () => {
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+  navbar.classList.remove('active');
+}
+//End of Navbar Section //
 
 
 
+// Start of Header Section //
 window.onscroll = () => {
 
   if(window.scrollY > 100) {
@@ -10,3 +22,4 @@ window.onscroll = () => {
   }
 
   }
+// End of Header Section //
